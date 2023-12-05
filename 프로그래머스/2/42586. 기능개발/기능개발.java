85b4c21c9789712ef9aed796aba2b31de0da82c7
hackertaco <imsa1224@korea.ac.kr>
2023-12-05 16:02:07 +0900

    [level 2] Title: 기능개발, Time: 25.61 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,6 +1,29 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
-        return answer;
+        Stack<Integer> stack = new Stack<>();
+        int[] durations = new int[progresses.length];
+        for(int i = 0; i < progresses.length; i++){
+            int duration = (100-progresses[i]) / speeds[i];
+            if((100-progresses[i]) % speeds[i] > 0) duration++;
+            durations[i] = duration;
+        }
+        int prev = 0;
+        int curr = 0;
+        while(curr < durations.length){
+            if(durations[prev] < durations[curr]){
+                stack.push(curr-prev);
+                prev = curr;
+                curr++;
+            }else if(durations[prev] >= durations[curr]){
+                curr++;
+            }
+            
+        }
+        if(prev < curr){
+            stack.push(curr-prev);
+        }
+        return stack.stream().mapToInt(i->i).toArray();
     }
 }
