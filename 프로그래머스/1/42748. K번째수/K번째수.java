d93106827874de073f6f198ec6269c9d91f33b29
hackertaco <imsa1224@korea.ac.kr>
2023-12-10 21:53:15 +0900

    [level 1] Title: K번째수, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,16 +1,41 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> list = new ArrayList<>();
-        
-        for(int[] command: commands){
-            int start = command[0];
-            int end = command[1];
-            System.out.println(array[end-1]);
-            int[] sliced = Arrays.copyOfRange(array, start-1, end);
-            Arrays.sort(sliced);
-            list.add(sliced[command[2]-1]);
+        int[] answer = new int[commands.length];
+        for(int i = 0; i<commands.length; i++){
+            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            quickSort(arr, 0, arr.length-1);
+            answer[i] = arr[commands[i][2]-1];
         }
-        return list.stream().mapToInt(i->i).toArray();
+        return answer;
     }
+    public void quickSort(int[] arr, int start, int end){
+        if (start >= end) return;
+    
+        int pivot = start;
+        int lo = start + 1;
+        int hi = end;
+    
+        while (lo <= hi) {
+          while (lo <= end && arr[lo] <= arr[pivot]) // 피벗보다 큰 값을 만날 때까지
+            lo++;
+          while (hi > start && arr[hi] >= arr[pivot]) // 피벗보다 작은 값을 만날 때까지
+            hi--;
+            
+          if (lo > hi) // 엇갈리면 피벗과 교체
+            swap(arr, hi, pivot);
+          else
+            swap(arr, lo, hi);// 엇갈리지 않으면 lo, hi 값 교체 
+          }
+	
+        quickSort(arr, start, hi - 1);
+        quickSort(arr, hi + 1, end);
+
+        }
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;        
+    }
+
 }
