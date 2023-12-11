005c96dcd113e695c42ee18799308e7d1a3cb33b
hackertaco <imsa1224@korea.ac.kr>
2023-12-11 20:48:43 +0900

    [level 2] Title: 주식가격, Time: 53.35 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,28 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Queue<Integer> priceQueue = new LinkedList<>();
-
-        for (int price : prices) {
-            priceQueue.add(price);
-        }
-
-        int idx = 0;
-        while (priceQueue.peek() != null) {
-            int value = priceQueue.poll();
-            answer[idx] = 0;
-
-            for (Integer integer : priceQueue) {
-                answer[idx]++;
-                if (value > integer) {
-                    break;
+        int[] answer = {};
+        Stack<Integer> time = new Stack<>();
+        Stack<Integer> count = new Stack<>();
+        
+        time.push(prices.length-1);
+        
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] >= prices[i-1]){
+                time.push(prices.length - 1 - i);
+            }else{
+                int prev = i - 1;
+                while(prev > -1 && prices[i] < prices[prev] && time.peek() > 1){
+                    count.push(time.pop()-1);
+                    prev--;
                 }
+                while(!count.isEmpty()){
+                    time.push(count.pop());
+                }
+                time.push(prices.length - 1 - i);
             }
-
-            idx++;
         }
-
-        return answer;
+        return time.stream().mapToInt(i->i).toArray();
     }
 }
