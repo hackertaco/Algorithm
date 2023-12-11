e9b8dc368988c65c263bf07127ba76a8f634a07e
hackertaco <imsa1224@korea.ac.kr>
2023-12-11 23:35:43 +0900

    [level 2] Title: 프로세스, Time: 1.05 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,37 +2,37 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        int[] copy = Arrays.copyOf(priorities, priorities.length);
-        Arrays.sort(copy);
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int c: copy){
-            stack.push(c);
+        Queue<Integer> queue =  new LinkedList<>();
+        for(int priority: priorities){
+            queue.add(priority);
         }
-        int pointer =0;
+        
+        int[] copied = Arrays.copyOf(priorities, priorities.length);
+        
+        Arrays.sort(copied, Collections.reverseOrder());
+        int curr = 0;
+        int target = priorities[location];
+        int idx = location;
+        List<Boolean> flags = new ArrayList<>();
+        Collections.fill(flags, false);
+        flags.add(location, true);
         while(true){
-            if(stack.peek() == priorities[pointer]){
-                if(pointer == location){
+            if(queue.poll() < copied[curr]){
+                queue.offer(queue.remove());
+                flags.put(flags.remove(0));
+            }else if(queue.poll() == copied[curr]){
+                answer++;
+                curr++;
+                queue.remove();
+                if(flags.get(curr)){
                     break;
-                }else{
-                    answer++;
-                    stack.pop();
-                    pointer = movePointer(pointer, priorities.length-1);
                 }
-            }else{
-                pointer = movePointer(pointer, priorities.length-1);
+                
             }
+            
         }
         
         
-        
-        return answer+1;
-    }
-    public int movePointer(int pointer, int length){
-        if(pointer == length){
-            return 0;
-        }else{
-            return ++pointer;
-        }
+        return answer;
     }
 }
